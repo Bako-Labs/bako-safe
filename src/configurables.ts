151a3ab66d4a8235ae1821f90c2488a467b30f3f
guimroque <guilhermemr_2017@hotmail.com>
2023-10-27 18:38:01 -0300

    chore(): environments

--- src/configurables.ts
+++ src/configurables.ts
@@ -1,9 +1,10 @@
 import { bn } from 'fuels';
+const { ENCODER, API_URL, REFETCH_TIMEOUT } = process.env;
 export const defaultConfigurable = {
-    encoder: 'fuel',
-    api_url: 'http://localhost:3333',
+    encoder: ENCODER || 'fuel',
+    api_url: API_URL || 'http://localhost:3333',
     provider: 'http://localhost:4000/graphql',
     gasPrice: bn(1_000_000),
     gasLimit: bn(100000),
-    refetchTimeout: 1000
+    refetchTimeout: REFETCH_TIMEOUT || 1000
 };
