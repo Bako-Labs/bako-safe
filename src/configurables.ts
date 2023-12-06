05486eb1060424ae936ccbc3fb8483180ad4f1b5
guimroque <guilhermemr_2017@hotmail.com>
2023-12-06 09:27:45 -0300

    chore(configurables): use stg environment

--- src/configurables.ts
+++ src/configurables.ts
@@ -1,13 +1,13 @@
 import { bn } from 'fuels';
 
 export const defaultConfigurable = {
-  //api_url: 'https://stg-api.bsafe.pro',
+  api_url: 'https://stg-api.bsafe.pro',
   //bsafe_url: 'http://app.bsafe.pro', // prd
   bsafe_url: 'https://bsafe-ui-git-staging-infinity-base.vercel.app/', // stg
-  //provider: 'https://beta-4.fuel.network/graphql',
-  api_url: 'http://localhost:3333',
+  provider: 'https://beta-4.fuel.network/graphql',
+  //api_url: 'http://localhost:3333',
   //bsafe_url: 'http://localhost:5173',
-  provider: 'http://localhost:4000/graphql',
+  //provider: 'http://localhost:4000/graphql',
 
   encoder: 'fuel',
   gasPrice: bn(100),
