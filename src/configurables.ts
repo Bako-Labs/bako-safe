85d50d26f2e5199930752ad90cf1b21449fcfe8a
guimroque <guilhermemr_2017@hotmail.com>
2023-12-11 09:41:37 -0300

    chore(conf): default variables

--- src/configurables.ts
+++ src/configurables.ts
@@ -2,12 +2,12 @@ import { bn } from 'fuels';
 
 export const defaultConfigurable = {
   //bsafe_url: 'http://app.bsafe.pro', // prd
-  api_url: 'https://stg-api.bsafe.pro',
-  bsafe_url: 'https://bsafe-ui-git-staging-infinity-base.vercel.app/', // stg
-  provider: 'https://beta-4.fuel.network/graphql',
   // api_url: 'http://localhost:3333',
   // bsafe_url: 'http://localhost:5174',
   // provider: 'http://localhost:4000/graphql',
+  api_url: 'https://stg-api.bsafe.pro',
+  bsafe_url: 'https://bsafe-ui-git-staging-infinity-base.vercel.app/', // stg
+  provider: 'https://beta-4.fuel.network/graphql',
   encoder: 'fuel',
   gasPrice: bn(100),
   gasLimit: bn(100000),
