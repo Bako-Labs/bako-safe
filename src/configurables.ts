bbd84e21d9cf6805adeabbdd3fa38bdb22f14991
guimroque <guilhermemr_2017@hotmail.com>
2023-12-14 21:07:58 -0300

    chore(environment): upgrade to prod

--- src/configurables.ts
+++ src/configurables.ts
@@ -1,13 +1,16 @@
 import { bn } from 'fuels';
 
 export const defaultConfigurable = {
-  //bsafe_url: 'http://app.bsafe.pro', // prd
   // api_url: 'http://localhost:3333',
   // bsafe_url: 'http://localhost:5174',
   // provider: 'http://localhost:4000/graphql',
-  api_url: 'https://stg-api.bsafe.pro',
-  bsafe_url: 'https://bsafe-ui-git-staging-infinity-base.vercel.app/', // stg
+  // stg
+  //api_url: 'https://stg-api.bsafe.pro',
+  //bsafe_url: 'https://bsafe-ui-git-staging-infinity-base.vercel.app/',
   provider: 'https://beta-4.fuel.network/graphql',
+  //prd
+  api_url: 'https://app.bsafe.pro/',
+  bsafe_url: 'https://app.bsafe.pro/',
   encoder: 'fuel',
   gasPrice: bn(100),
   gasLimit: bn(100000),
