ca907a28c1b5293e3d806b31f3257f59bb883bac
pedroanastacio <pedro.anastacio013@gmail.com>
2024-05-13 10:10:11 -0300

    feat(vault): add signers count param to new vault util function

--- packages/sdk/test/utils/defaultVault.ts
+++ packages/sdk/test/utils/defaultVault.ts
@@ -10,10 +10,11 @@ export const newVault = async (
   fuelProvider: Provider,
   auth?: IBakoSafeAuth,
   reason?: keyof typeof VALUES_DEFAULT_TO_MUL,
+  signersCount: number = signers.length,
 ) => {
   const VaultPayload: IPayloadVault = {
     configurable: {
-      SIGNATURES_COUNT: 3,
+      SIGNATURES_COUNT: signersCount,
       SIGNERS: signers,
       network: fuelProvider.url,
     },
