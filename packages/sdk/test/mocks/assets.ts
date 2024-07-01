4658d2c8edd4f191820bd9c758787782825cd54d
pedroanastacio <pedro.anastacio013@gmail.com>
2024-07-01 17:43:16 -0300

    chore(assets): add new BTC and USDC ids

--- packages/sdk/test/mocks/assets.ts
+++ packages/sdk/test/mocks/assets.ts
@@ -2,8 +2,8 @@ import { bn } from 'fuels';
 
 export const assets = {
   ETH: '0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07', // local node has configured with this baseAssetId
-  BTC: '0xfe3f96763e20e5d168037065a3bc65b751df7a038fd9297cc1cb5c740fd1c170',
-  USDC: '0xda1fb840452bba3ab65a11f782902e7286fc44b94a9b85059dd7f8b68bf371d4',
+  BTC: '0xccceae45a7c23dcd4024f4083e959a0686a191694e76fa4fb76c449361ca01f7',
+  USDC: '0xfed3ee85624c79cb18a3a848092239f2e764ed6b0aa156ad10a18bfdbe74269f',
   UNI: '0xb3238af388ac05188e342b1801db79d358e4a162734511316c937b00c8687fe9',
 };
 
