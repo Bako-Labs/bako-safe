ec34b5026b824a2df62c925c871287bb5bbb5f56
guimroque <guilhermemr_2017@hotmail.com>
2024-08-12 00:18:46 -0300

    wip: webauthn signature mock

--- packages/tests/fuels.config.ts
+++ packages/tests/fuels.config.ts
@@ -1,12 +1,13 @@
 import { createConfig } from 'fuels';
 
 import dotenv from 'dotenv';
+import { accounts, networks } from './src/mocks';
 
 dotenv.config();
 
 export default createConfig({
   contracts: ['./src/sway/contract'],
-  scripts: ['./src/sway/scripts'],
+  scripts: ['./src/sway/script'],
   providerUrl: process.env.PROVIDER_URL,
   privateKey: process.env.PRIVATE_KEY,
   output: './src/types/sway',
--- packages/tests/src/utils/signin.ts
+++ packages/tests/src/utils/signin.ts
@@ -7,8 +7,8 @@ import { LocalProvider } from './provider';
 export const signin = async (
   tx_hash: string,
   account: 'FULL' | 'USER_1' | 'USER_2' | 'USER_3' | 'USER_4' | 'USER_5',
-  auth?: IBakoSafeAuth,
-  BakoSafeTransactionId?: string,
+  // auth?: IBakoSafeAuth,
+  // BakoSafeTransactionId?: string,
 ) => {
   const fuelProvider = new LocalProvider();
 
@@ -23,11 +23,11 @@ export const signin = async (
   //   //bite: new BigNumberCoder(`u64`).encode(2).toString(),
   // });
 
-  if (!!auth && BakoSafeTransactionId) {
-    const acc = Address.fromString(accounts[account].address).toString();
-    const serviceTransactions = new TransactionService(auth);
-    return await serviceTransactions.sign(BakoSafeTransactionId, acc, tx);
-  }
+  // if (!!auth && BakoSafeTransactionId) {
+  //   const acc = Address.fromString(accounts[account].address).toString();
+  //   const serviceTransactions = new TransactionService(auth);
+  //   return await serviceTransactions.sign(BakoSafeTransactionId, acc, tx);
+  // }
 
   //return [tx, concat([new BigNumberCoder(`u64`).encode(1), tx])];
   return tx;
