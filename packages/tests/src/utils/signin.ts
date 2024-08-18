a5950454b9a283e3e180247e8d1834da2889f547
guimroque <guilhermemr_2017@hotmail.com>
2024-08-18 11:27:50 -0300

    fix: remove unused tests

--- packages/tests/src/utils/signin.ts
+++ packages/tests/src/utils/signin.ts
@@ -7,8 +7,6 @@ import { LocalProvider } from './provider';
 export const signin = async (
   tx_hash: string,
   account: 'FULL' | 'USER_1' | 'USER_2' | 'USER_3' | 'USER_4' | 'USER_5',
-  // auth?: IBakoSafeAuth,
-  // BakoSafeTransactionId?: string,
 ) => {
   const fuelProvider = new LocalProvider();
 
@@ -17,18 +15,6 @@ export const signin = async (
     fuelProvider,
   );
   const tx = await signer.signMessage(tx_hash);
-  // console.log('[ASSINATURAS]', {
-  //   normal: tx,
-  //   prefix: concat([new BigNumberCoder(`u64`).encode(1), tx]),
-  //   //bite: new BigNumberCoder(`u64`).encode(2).toString(),
-  // });
 
-  // if (!!auth && BakoSafeTransactionId) {
-  //   const acc = Address.fromString(accounts[account].address).toString();
-  //   const serviceTransactions = new TransactionService(auth);
-  //   return await serviceTransactions.sign(BakoSafeTransactionId, acc, tx);
-  // }
-
-  //return [tx, concat([new BigNumberCoder(`u64`).encode(1), tx])];
   return tx;
 };
