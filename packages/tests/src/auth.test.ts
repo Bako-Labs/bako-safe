d2d5bcb7de85da4a91383f603d64d10bb2480167
guimroque <guilhermemr_2017@hotmail.com>
2024-08-23 01:12:17 -0300

    test: provider auth actions

--- packages/tests/src/auth.test.ts
+++ packages/tests/src/auth.test.ts
@@ -1,6 +1,22 @@
-import { accounts, networks } from './mocks';
-import { VaultProvider } from '../../sdk/src/modules/provider/VaultProvider';
-import { Wallet } from 'fuels';
+import { Address, Wallet } from 'fuels';
+
+import { accounts, assets, networks } from './mocks';
+import { VaultProvider, Vault } from '../../sdk/src/modules/';
+import { sendCoins } from './utils';
+
+// User auth resources:
+//    - auth -> ok (fuel, webauthn?)
+//    - get workspaces -> ok
+//    - create workspace -> ?
+//    - get userInfo -> ok
+//    - vault store -> ok
+//    - vault recover -> ok
+//    - tx store
+//    - tx recover
+//    - tx send
+//    - tx wait
+
+// type returned data of services
 
 describe('[AUTH]', () => {
   it('Should authenticate with a token', async () => {
@@ -29,5 +45,123 @@ describe('[AUTH]', () => {
     expect(vaultProvider.options.challenge).toBe(challenge);
     expect(tokens).toBeDefined();
     expect(tokens.length).toBeGreaterThan(0);
+  });
+
+  it('Should find my workspaces', async () => {
+    const address = accounts['USER_1'].account;
+
+    const challenge = await VaultProvider.setup({
+      address,
+    });
+
+    const token = await Wallet.fromPrivateKey(
+      accounts['USER_1'].privateKey,
+    ).signMessage(challenge);
+
+    const vaultProvider = await VaultProvider.create(networks['LOCAL'], {
+      address,
+      challenge,
+      token,
+    });
+
+    const workspaces = await vaultProvider.service.getWorkspaces();
+    expect(workspaces).toBeDefined();
+    expect(workspaces.length).toBeGreaterThanOrEqual(0);
+  });
+
+  it('Should get infos of current auth', async () => {
+    const address = accounts['USER_1'].account;
+
+    const challenge = await VaultProvider.setup({
+      address,
+    });
+
+    const token = await Wallet.fromPrivateKey(
+      accounts['USER_1'].privateKey,
+    ).signMessage(challenge);
+
+    const vaultProvider = await VaultProvider.create(networks['LOCAL'], {
+      address,
+      challenge,
+      token,
+    });
+
+    const authInfo = await vaultProvider.service.authInfo();
+
+    expect(authInfo).toBeDefined();
+    expect(authInfo.address).toBe(address);
+    expect(authInfo.workspace).toBeDefined();
+    expect(authInfo.onSingleWorkspace).toBe(true);
+  });
+
+  it('Should store a vault', async () => {
+    const address = accounts['USER_1'].account;
+
+    const challenge = await VaultProvider.setup({
+      address,
+    });
+
+    const token = await Wallet.fromPrivateKey(
+      accounts['USER_1'].privateKey,
+    ).signMessage(challenge);
+
+    const vaultProvider = await VaultProvider.create(networks['LOCAL'], {
+      address,
+      challenge,
+      token,
+    });
+
+    const predicate = new Vault(vaultProvider, {
+      SIGNATURES_COUNT: 1,
+      SIGNERS: [address],
+      HASH_PREDICATE: Address.fromRandom().toB256(),
+    });
+
+    const saved = await predicate.store();
+
+    expect(saved).toBeDefined();
+    expect(saved.predicateAddress).toBeDefined();
+    expect(saved.predicateAddress.length).toBeGreaterThan(0);
+    expect(saved.predicateAddress).toBe(predicate.address.toB256());
+  });
+
+  it('Should recover a vault', async () => {
+    const address = accounts['USER_1'].account;
+
+    const challenge = await VaultProvider.setup({
+      address,
+    });
+
+    const token = await Wallet.fromPrivateKey(
+      accounts['USER_1'].privateKey,
+    ).signMessage(challenge);
+
+    const vaultProvider = await VaultProvider.create(networks['LOCAL'], {
+      address,
+      challenge,
+      token,
+    });
+
+    const predicate = new Vault(vaultProvider, {
+      SIGNATURES_COUNT: 1,
+      SIGNERS: [address],
+      HASH_PREDICATE: Address.fromRandom().toB256(),
+    });
+
+    const saved = await predicate.store();
+
+    const balanceValue = '0.1';
+    await sendCoins(predicate.address.toB256(), balanceValue, assets['ETH']);
+
+    const recover = await Vault.stored(saved.predicateAddress, vaultProvider);
+
+    const predicateBalance = await predicate.getBalance(assets['ETH']);
+    const recoverBalance = await recover.getBalance(assets['ETH']);
+
+    // 18 is max of decimals to represent value
+    expect(predicate.address.toB256()).toBe(recover.address.toB256());
+    expect(predicateBalance.formatUnits(18)).toBe(
+      recoverBalance.formatUnits(18),
+    );
   });
 });
