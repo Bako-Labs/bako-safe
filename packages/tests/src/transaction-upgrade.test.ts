a620a77a44e0580132058b0b116e0e08a4fae343
luisburigo <guga.burigo29@outlook.com>
2024-09-11 23:57:17 -0300

    test(transactions): implement tests for upgrade transaction

--- packages/tests/src/transaction-upgrade.test.ts
+++ packages/tests/src/transaction-upgrade.test.ts
@@ -1,13 +1,10 @@
+import { BytesLike, hexlify, UpgradeTransactionRequest, Provider } from 'fuels';
 import {
-  BytesLike,
-  hexlify,
-  Predicate,
-  Provider,
-  UpgradeTransactionRequest,
-  Wallet,
-  WalletUnlocked,
-} from 'fuels';
-import { accounts, assets, PRIVILEGED_CONSENSUS_BYTECODE } from './mocks';
+  accounts,
+  assets,
+  PRIVILEGED_CONSENSUS_BYTECODE,
+  GAS_COSTS_CONSENSUS_BYTECODE,
+} from './mocks';
 import { randomBytes } from 'crypto';
 import { launchTestNode } from 'fuels/test-utils';
 import { bakoCoder, SignatureType, Vault } from '../../sdk/src';
@@ -16,17 +13,16 @@ import { expect } from '@jest/globals';
 
 const baseAssetId = assets['ETH'];
 
-const setupTestNode = async (predicate: Vault) => {
-  const wallet = Wallet.fromPrivateKey(accounts['USER_1'].privateKey);
-  const predicateAddress = predicate.address.toB256();
+const setupTestNode = async (vault: Vault) => {
+  const predicateAddress = vault.address.toB256();
   const consensusParameters = {
     V1: {
-      privileged_address: wallet.address.toB256(),
+      privileged_address: predicateAddress,
       base_asset_id: baseAssetId,
     },
   };
   const coin = {
-    owner: wallet.address.toB256(),
+    owner: predicateAddress,
     amount: 1000000000,
     asset_id: baseAssetId,
     tx_id: hexlify(randomBytes(32)),
@@ -49,16 +45,13 @@ const setupTestNode = async (predicate: Vault) => {
       count: 1,
     },
   });
-  // @ts-ignore
-  predicate.provider = provider;
-  wallet.provider = provider;
+  vault.provider = provider;
 
   return {
-    provider,
-    predicate,
+    vault,
     wallets,
-    wallet,
     cleanup,
+    provider,
     [Symbol.dispose]: cleanup,
   };
 };
@@ -84,9 +77,14 @@ const upgradeConsensusParameters = async (
 
 describe('[Transaction Upgrade]', () => {
   let node: Awaited<ReturnType<typeof launchTestNode>>;
+  let vault: Vault;
 
   beforeEach(async () => {
     node = await launchTestNode();
+    vault = new Vault(node.provider, {
+      SIGNATURES_COUNT: 1,
+      SIGNERS: [accounts['USER_1'].address],
+    });
   });
 
   afterEach(() => {
@@ -94,47 +92,49 @@ describe('[Transaction Upgrade]', () => {
   });
 
   it('should correctly upgrade privileged address of chain', async () => {
-    // @ts-ignore
-    const vault = new Vault(node.provider, {
-      SIGNATURES_COUNT: 1,
-      SIGNERS: [accounts['USER_1'].address],
-    });
-    //
-    using asdasd = await setupTestNode(vault);
-    const { wallet } = asdasd;
-    //
-    //   const { isTypeUpgrade, isStatusSuccess, ...rest } =
-    //     await upgradeConsensusParameters(vault, PRIVILEGED_CONSENSUS_BYTECODE);
-    //   expect(isTypeUpgrade).toBeTruthy();
-    //   expect(isStatusSuccess).toBeTruthy();
-    //
-    //   await expect(() =>
-    //     upgradeConsensusParameters(vault, PRIVILEGED_CONSENSUS_BYTECODE),
-    //   ).rejects.toThrowError(/Validity\(TransactionUpgradeNoPrivilegedAddress\)/);
-    const upgradeConsensusParameters = async (
-      wallet: WalletUnlocked,
-      bytecode: BytesLike,
-    ) => {
-      const request = new UpgradeTransactionRequest();
-      request.addConsensusParametersUpgradePurpose(bytecode);
+    using node = await setupTestNode(vault);
+    const { vault: privilegedVault } = node;
 
-      const cost = await wallet.getTransactionCost(request);
-      request.maxFee = cost.maxFee;
-      await wallet.fund(request, cost);
-
-      const response = await wallet.sendTransaction(request);
-      return response.waitForResult();
-    };
-
+    // Upgrade privileged address to a new address
     const { isStatusSuccess, isTypeUpgrade } = await upgradeConsensusParameters(
-      wallet,
+      privilegedVault,
       PRIVILEGED_CONSENSUS_BYTECODE,
     );
     expect(isStatusSuccess).toBeTruthy();
     expect(isTypeUpgrade).toBeTruthy();
 
+    await privilegedVault.provider.fetchChainAndNodeInfo();
+
+    // Check if the privileged address has been updated
     await expect(() =>
-      upgradeConsensusParameters(wallet, PRIVILEGED_CONSENSUS_BYTECODE),
+      upgradeConsensusParameters(
+        privilegedVault,
+        PRIVILEGED_CONSENSUS_BYTECODE,
+      ),
     ).rejects.toThrowError(/Validity\(TransactionUpgradeNoPrivilegedAddress\)/);
+  });
+
+  it('should correctly upgrade gas costs of chain', async () => {
+    using node = await setupTestNode(vault);
+    const { vault: privilegedVault, provider } = node;
+
+    const {
+      consensusParameters: { gasCosts: gasCostsBefore },
+    } = provider.getChain();
+
+    // Upgrade gas costs of chain to free
+    const { isStatusSuccess, isTypeUpgrade } = await upgradeConsensusParameters(
+      privilegedVault,
+      GAS_COSTS_CONSENSUS_BYTECODE,
+    );
+    expect(isStatusSuccess).toBeTruthy();
+    expect(isTypeUpgrade).toBeTruthy();
+
+    const {
+      consensusParameters: { gasCosts: gasCostsAfter },
+    } = await provider.fetchChain();
+
+    // Check if gas costs have been updated
+    expect(gasCostsAfter).not.toEqual(gasCostsBefore);
   });
 });
