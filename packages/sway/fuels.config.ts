34f9442cff05d82312ec587118b3639cf3cc0111
luisburigo <guga.burigo29@outlook.com>
2024-10-07 09:20:52 -0300

    chore: add env

--- packages/sway/fuels.config.ts
+++ packages/sway/fuels.config.ts
@@ -4,10 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const { PROVIDER_URL: providerUrl, PRIVATE_KEY: privateKey } = process.env;
+
 export default createConfig({
+  privateKey,
+  providerUrl,
   predicates: ['./src/predicate'],
   forcBuildFlags: ['--release'],
   output: '../sdk/src/sway',
-  providerUrl: '',
-  privateKey: '0x0001',
 });
