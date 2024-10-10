484f2100a9b6a690be9d895f58d8b07e5a65d45f
guimroque <guilhermemr_2017@hotmail.com>
2024-10-09 22:25:09 -0300

    fix: bakopredicateloader import

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,3 +2,11 @@ export * from './utils';
 export * from './modules';
 export * from './modules/service';
 export * from './utils/assets';
+
+/**
+ * we have this package here because on build ;
+ * the fuels replace not include export * from './sway/predicates/BakoPredicateLoader' on
+ * the fuels.config.ts file destination
+ *
+ * */
+export * from './sway/predicates/BakoPredicateLoader';
