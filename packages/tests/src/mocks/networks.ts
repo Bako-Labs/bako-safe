93151f899bb898385f0b902a8bff89573d68f0e4
guimroque <guilhermemr_2017@hotmail.com>
2024-10-22 20:28:53 -0300

    chore: fix networks

--- packages/tests/src/mocks/networks.ts
+++ packages/tests/src/mocks/networks.ts
@@ -1,7 +1,6 @@
 export const networks: { [key: string]: string } = {
-  BETA_5: 'https://beta-5.fuel.network/graphql',
   LOCAL: 'http://127.0.0.1:4000/v1/graphql',
-  DEVNET: 'https://testnet.fuel.network/v1/graphql',
+  TESNET: 'https://testnet.fuel.network/v1/graphql',
 };
 
 export type INetworkKeys = keyof typeof networks;
