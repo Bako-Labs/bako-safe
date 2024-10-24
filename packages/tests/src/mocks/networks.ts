d54137d43cc84f068c8ade6a724b0e9f7ceadaec
guimroque <guilhermemr_2017@hotmail.com>
2024-10-24 18:47:30 -0300

    chore: update networks

--- packages/tests/src/mocks/networks.ts
+++ packages/tests/src/mocks/networks.ts
@@ -1,6 +1,6 @@
 export const networks: { [key: string]: string } = {
-  LOCAL: 'http://127.0.0.1:4000/v1/graphql',
   TESNET: 'https://testnet.fuel.network/v1/graphql',
+  MAINNET: 'https://mainnet.fuel.network/v1/graphql',
 };
 
 export type INetworkKeys = keyof typeof networks;
