35fec1f8e185270dc96f65a23117461fb87619dd
pedroanastacio <pedro.anastacio013@gmail.com>
2024-11-05 18:57:43 -0300

    fix(predicate): create predicate sending version

--- packages/sdk/src/modules/provider/BakoProvider.ts
+++ packages/sdk/src/modules/provider/BakoProvider.ts
@@ -119,7 +119,7 @@ export class BakoProvider extends Provider {
       predicateAddress: vault.address.toB256(),
       configurable: JSON.stringify(vault.configurable),
       provider: this.url,
-      version: vault.version,
+      version: vault.predicateVersion,
     };
 
     const predicate = await this.service.createPredicate(payload);
